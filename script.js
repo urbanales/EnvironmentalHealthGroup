de06dd7a519512f59ad496bd92b028b51a2d2519
Aleš Urban <90908091+urbanales@users.noreply.github.com>
2026-01-06 11:42:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function openTab(tabId) {
+  const contents = document.querySelectorAll('.tab-content');
+  const tabs = document.querySelectorAll('.tab');
+
+  contents.forEach(c => c.classList.remove('active'));
+  tabs.forEach(t => t.classList.remove('active'));
+
+  document.getElementById(tabId).classList.add('active');
+  event.target.classList.add('active');
+}
 
